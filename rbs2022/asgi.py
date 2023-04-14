b0da5b651644f3049e3d34867a3ed5eafbef1fa0
G-Milzink <G.Milzink@gmail.com>
2023-04-14 12:22:52 +0000

    create rbs2022 project

---
+++ rbs2022/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for rbs2022 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rbs2022.settings')
+
+application = get_asgi_application()
