f3a9af807f2281b5b3e92bed1c9f4ddad4fb1981
G-Milzink <G.Milzink@gmail.com>
2023-04-14 13:55:59 +0000

    change allowed host

--- rbs2022/settings.py
+++ rbs2022/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rbs2022.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['ci-pp4-rbs.herokuapp.com', "localhost"]
 
 
 # Application definition
