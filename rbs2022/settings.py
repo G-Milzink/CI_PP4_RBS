12aac1f729c95cd107c2b29d4546d185ee9ec1c8
G-Milzink <G.Milzink@gmail.com>
2023-04-14 13:11:57 +0000

    tweaks to settings.py

--- rbs2022/settings.py
+++ rbs2022/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 import dj_database_url
+
 if os.path.exists:
     import env
 
