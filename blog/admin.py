043b484d16822681b4bf7116194cd2813e8ae3eb
G-Milzink <G.Milzink@gmail.com>
2023-04-15 13:47:21 +0000

    setup blog/comment admin functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Blogpost
+from .models import Blogpost, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
-# Register your models here.
-admin.site.register(Blogpost)
+@admin.register(Blogpost)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
