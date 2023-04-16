a9d1e4df1f724a2da3eac96559356ce1ff0512fb
G-Milzink <G.Milzink@gmail.com>
2023-04-16 13:04:05 +0000

    setup base/blog templates/display blogposts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.core.paginator import Paginator
 from .models import Blogpost
 
 
@@ -8,3 +9,15 @@ class PostList(generic.ListView):
     queryset = Blogpost.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
     paginate_by = 4
+
+    def get(self, request, *args, **kwargs):
+        """
+        This view renders the blog page and also all published posts
+        """
+        posts = Blogpost.objects.all()
+        paginator = Paginator(Blogpost.objects.all(), 4)
+        page = request.GET.get('page')
+        post_list = paginator.get_page(page)
+
+        return render(
+            request, 'blog.html',  {'posts': posts, 'post_list': post_list})
