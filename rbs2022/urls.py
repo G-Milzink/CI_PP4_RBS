961255c38d3840b6698f55861e8be233e2a99590
G-Milzink <G.Milzink@gmail.com>
2023-04-19 16:13:14 +0000

    reqgister allauth urls

--- rbs2022/urls.py
+++ rbs2022/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('acounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('blog/', include('blog.urls'), name='blog_urls')
 ]
