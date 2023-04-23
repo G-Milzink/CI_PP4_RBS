6b0836a8a9ea48bcb9c3b12de0e002acb9dd04b2
G-Milzink <G.Milzink@gmail.com>
2023-04-23 15:27:08 +0000

    remove contact-form view code/faulty, need to redo

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import ContactForm
