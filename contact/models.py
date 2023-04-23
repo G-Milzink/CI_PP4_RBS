cfd42988db2b59bf1122495a8d50c23a542d7413
G-Milzink <G.Milzink@gmail.com>
2023-04-23 12:15:17 +0000

    define contact model and migrate

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Contact(models.Model):
+    message_id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="contact_user",
+        null=True
+        )
+    name = models.CharField(
+        max_length=50,
+        null=True
+        )
+    email = models.EmailField(
+        max_length=254,
+        default=""
+        )
+    message = models.TextField()
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return self.name
