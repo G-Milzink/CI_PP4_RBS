f69ba255269452b0295b514e2a60abf33c74bbfb
G-Milzink <G.Milzink@gmail.com>
2023-04-25 19:48:08 +0000

    define drinks menu view

--- menu/views.py
+++ menu/views.py
@@ -6,3 +6,9 @@ def food_menu(request):
 
     food_items = FoodItem.objects.all()
     return render(request, 'food.html', {'food_items': food_items})
+
+
+def drinks_menu(request):
+
+    food_items = FoodItem.objects.all()
+    return render(request, 'drinks.html', {'drinks_items': food_items})
