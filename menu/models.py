417ee6a928c3a0eb8a9de8c55343d442d73ede87
G-Milzink <G.Milzink@gmail.com>
2023-04-25 19:42:04 +0000

    define drinks db-model and make migrations

--- menu/models.py
+++ menu/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 FOOD_CATEGORY = ((0, 'New'), (1, 'Starters'), (2, 'Mains'), (3, 'Desserts'))  # noqa
-DRINK_CATEGORY = ((0, 'New'), (1, 'Beers'), (2, 'Wines'), (3, 'Non-Alchocolic'))  # noqa
+DRINKS_CATEGORY = ((0, 'New'), (1, 'Beers'), (2, 'Wines'), (3, 'Non-Alchocolic'))  # noqa
 
 
 class FoodItem(models.Model):
@@ -12,6 +12,22 @@ class FoodItem(models.Model):
     item_description = models.CharField(max_length=250, unique=True)
     item_price = models.FloatField()
     item_category = models.IntegerField(choices=FOOD_CATEGORY, default=0)
+    item_available = models.BooleanField(default=False)
+
+    class meta:
+        ordering = ['-item_category']
+
+    def __str__(self):
+        return self.item_name
+
+
+class DrinkItem(models.Model):
+
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=50, unique=True)
+    item_description = models.CharField(max_length=250, unique=True)
+    item_price = models.FloatField()
+    item_category = models.IntegerField(choices=DRINKS_CATEGORY, default=0)
     item_available = models.BooleanField(default=False)
 
     class meta:
