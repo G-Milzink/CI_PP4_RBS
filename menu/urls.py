bc673f0f3e7d675916f408f918584aca5d8e26c6
G-Milzink <G.Milzink@gmail.com>
2023-04-25 20:12:06 +0000

    setup bite template/register url

--- menu/urls.py
+++ menu/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('food', views.food_menu, name='food'),
     path('drinks', views.drinks_menu, name='drinks'),
+    path('bites', views.bites_menu, name='bites')
 ]
