47bbe7fc3b6c519d1eacfdf6b70ee76e7fcd2a70
G-Milzink <G.Milzink@gmail.com>
2023-04-25 10:43:36 +0000

    create food.html/ setup boilerplate

--- menu/views.py
+++ menu/views.py
@@ -5,4 +5,4 @@ from .models import FoodItem
 def food_menu(request):
 
     food_items = FoodItem.objects.all()
-    return render(request, 'menu/food_menu.html', {'food_items'})
+    return render(request, 'menu/food.html', {'food_items'})
