4af5f7b4242a9844bf9917028fb504b0326b4480
G-Milzink <G.Milzink@gmail.com>
2023-04-25 10:39:11 +0000

    create food_item view

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import FoodItem
 
-# Create your views here.
+
+def food_menu(request):
+
+    food_items = FoodItem.objects.all()
+    return render(request, 'menu/food_menu.html', {'food_items'})
