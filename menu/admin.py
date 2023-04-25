0764357364f2bf6dfece8200e5bbb3659ed75918
G-Milzink <G.Milzink@gmail.com>
2023-04-25 19:55:04 +0000

    import DrinkItem model

--- menu/admin.py
+++ menu/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import FoodItem
+from .models import FoodItem, DrinkItem
 
 
-@admin.register(FoodItem)
+@admin.register(DrinkItem)
 class FoodAdmin(SummernoteModelAdmin):
     list_display = ('item_name', 'item_category', 'item_price', 'item_available')  # noqa
     search_fields = ('item_name', 'item_description')
     list_filter = ('item_category', 'item_available')
     summernote_fields = ('item_description')
 
-@admin.register(DrinksItem)
+
+@admin.register(FoodItem)
 class FoodAdmin(SummernoteModelAdmin):
     list_display = ('item_name', 'item_category', 'item_price', 'item_available')  # noqa
     search_fields = ('item_name', 'item_description')
