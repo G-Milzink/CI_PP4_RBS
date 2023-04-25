df25bb2dad0fa1d917ff9e99fd27fe82452f2637
G-Milzink <G.Milzink@gmail.com>
2023-04-25 10:27:31 +0000

    define food_item db-model and migrate

--- contact/admin.py
+++ contact/admin.py
@@ -5,6 +5,6 @@ from .models import Contact
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_filter = ('user', 'name', 'email', 'created_on')
-    list_display = ('message_id', 'user', 'name', 'message', 'created_on')
+    list_display = ('user', 'name', 'message', 'created_on')
     search_fields = ['name', 'created_on']
     list_filter = ('created_on',)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+FOOD_CATEGORY = ((0, 'New'), (1, 'Starters'), (2, 'Mains'), (3, 'Desserts'), (4, 'Bites'))
+DRINK_CATEGORY = ((0, 'New'), (1, 'Beers'), (2, 'Wines'), (3, 'Non-Alchocolic'))
+
+
+class FoodItem(models.Model):
+
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=50, unique=True)
+    item_description = models.CharField(max_length=250, unique=True)
+    item_price = models.FloatField()
+    item_category = models.IntegerField(choices=FOOD_CATEGORY, default=0)
+    item_available = models.BooleanField(default=False)
+
+    class meta:
+        ordering = ['-item_category']
+
+    def __str__(self):
+        return self.item_name
