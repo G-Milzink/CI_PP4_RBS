544294461d495ed3df5a72a8f5a294c9d232079d
G-Milzink <G.Milzink@gmail.com>
2023-04-25 19:18:28 +0000

    temporarily disable QA

--- menu/admin.py
+++ menu/admin.py
@@ -5,7 +5,7 @@ from .models import FoodItem
 
 @admin.register(FoodItem)
 class FoodAdmin(SummernoteModelAdmin):
-    list_display = ('item_name', 'item_category', 'item_price', 'item_available')
+    list_display = ('item_name', 'item_category', 'item_price', 'item_available')  # noqa
     search_fields = ('item_name', 'item_description')
     list_filter = ('item_category', 'item_available')
     summernote_fields = ('item_description')
