65ca49f74a935384c9f79cda6a0b2df93625fb24
G-Milzink <G.Milzink@gmail.com>
2023-04-25 11:18:19 +0000

    add starter section to food.html template

--- menu/admin.py
+++ menu/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django_summernote import SummnoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import FoodItem
 
 
 @admin.register(FoodItem)
-class FoodAdmin(SummnoteModelAdmin):
+class FoodAdmin(SummernoteModelAdmin):
     list_display = ('item_name', 'item_category', 'item_price', 'item_available')
     search_fields = ('item_name', 'item_description')
     list_filter = ('item_category', 'item_available')
