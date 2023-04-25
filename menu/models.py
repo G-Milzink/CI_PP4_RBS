e0c0ea707ee152a172e50dd549ba19d14be7ea97
G-Milzink <G.Milzink@gmail.com>
2023-04-25 19:45:37 +0000

    define bites db-model and make migrations

--- menu/models.py
+++ menu/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 FOOD_CATEGORY = ((0, 'New'), (1, 'Starters'), (2, 'Mains'), (3, 'Desserts'))  # noqa
 DRINKS_CATEGORY = ((0, 'New'), (1, 'Beers'), (2, 'Wines'), (3, 'Non-Alchocolic'))  # noqa
+BITES_CATEGORY = ((0, "New"), (1, 'Assorted'), (2, 'Vegetarian'))
 
 
 class FoodItem(models.Model):
@@ -28,6 +29,22 @@ class DrinkItem(models.Model):
     item_description = models.CharField(max_length=250, unique=True)
     item_price = models.FloatField()
     item_category = models.IntegerField(choices=DRINKS_CATEGORY, default=0)
+    item_available = models.BooleanField(default=False)
+
+    class meta:
+        ordering = ['-item_category']
+
+    def __str__(self):
+        return self.item_name
+
+
+class BitesItem(models.Model):
+
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=50, unique=True)
+    item_description = models.CharField(max_length=250, unique=True)
+    item_price = models.FloatField()
+    item_category = models.IntegerField(choices=BITES_CATEGORY, default=0)
     item_available = models.BooleanField(default=False)
 
     class meta:
