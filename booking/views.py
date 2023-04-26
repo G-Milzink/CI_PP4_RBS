5d8d29049d558960e4412df91a04c0cb1aa03fed
G-Milzink <G.Milzink@gmail.com>
2023-04-26 14:53:56 +0000

    define function to retreive user email

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def get_user_instance(request):
+
+    user_email = request.user.email
+    user = User.objects.filter(email=user_email).first()
+    return user
