e40d75a7fabea76700615ac1ae933a5c2e6b5827
G-Milzink <G.Milzink@gmail.com>
2023-04-26 13:28:13 +0000

    setup BookingForm

---
+++ booking/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from datetime import datetime
+from datetime import date
+from crispy_forms.helper import FormHelper
+from phonenumber_field.formfields import PhoneNumberField
+from .models import Booking
+
+
+class BookingForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+    booking_date = forms.DateField()
+
+    phone = PhoneNumberField(blank=True)
+
+    class Meta:
+        model = Booking
+        fields = (
+            'name',
+            'phone',
+            'email',
+            'nr_of_guests',
+            'table',
+            'booking_date',
+            'booking_time')
