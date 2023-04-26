7ba4b1a0aea0abefd6cb085d2ad885e5f38dede9
G-Milzink <G.Milzink@gmail.com>
2023-04-26 15:37:25 +0000

    remove table model from admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
 from .models import Table, Booking
-
-
-@admin.register(Table)
-class TableAdmin(admin.ModelAdmin):
-    list_display = ('table_id', 'table_name', 'nr_of_seats')
