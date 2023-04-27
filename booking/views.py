2dfd8e7373a1b4b644fdde8336ff7acdd9d2ac56
G-Milzink <G.Milzink@gmail.com>
2023-04-27 16:57:06 +0000

    add to allmybookings template,

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.views import generic, View
 from django.contrib import messages
