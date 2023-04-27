8dd4e5436a0e616926213117cd6cbbb6c5216141
G-Milzink <G.Milzink@gmail.com>
2023-04-27 11:48:08 +0000

    improve formatting

--- contact/models.py
+++ contact/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Contact(models.Model):
     message_id = models.AutoField(primary_key=True)
     created_on = models.DateTimeField(auto_now_add=True)
