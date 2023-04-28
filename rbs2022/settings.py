c6ce16c046c20524c37afd506f1bc15da9019bc3
G-Milzink <G.Milzink@gmail.com>
2023-04-28 13:06:24 +0000

    style password reset templates

--- rbs2022/settings.py
+++ rbs2022/settings.py
@@ -172,7 +172,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SMTP Configuraion
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
