31307c7369e077ab0078ce7970c260d40e639065
G-Milzink <G.Milzink@gmail.com>
2023-05-02 11:15:18 +0000

    test-deployment commit

--- rbs2022/settings.py
+++ rbs2022/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "True"
+DEBUG = "False"
 
 ALLOWED_HOSTS = ['ci-pp4-rbs.herokuapp.com', 'localhost']
 
